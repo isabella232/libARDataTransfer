f62d4344696eea0e0b9ac1285cc9cca7fd21a6bb
david.flattin <david.flattin.ext@parrot.com>
2014-03-27 16:13:02 +0100

    missing available media

---
+++ JNI/java/com/parrot/arsdk/ardatatransfer/ARDataTransferMediasDownloaderAvailableMediaListener.java
@@ -0,0 +1,19 @@
+
+package com.parrot.arsdk.ardatatransfer;
+
+/**
+ * ARDataTransfer MediasDownloader AvailableMediaListener
+ * @author david.flattin.ext@parrot.com
+ * @date 27/01/2014
+ */
+public interface ARDataTransferMediasDownloaderAvailableMediaListener
+{
+    /**
+     * Gives the ARDataTransferMedia media available status
+     * @param arg Object complete Listener arg
+     * @param media {@link ARDataTransferMedia} media available
+     * @param error ARDATATRANSFER_OK if success, else an {@link ARDATATRANSFER_ERROR_ENUM} error code
+     * @return void
+     */
+    void didMediaAvailable(Object arg, ARDataTransferMedia media);
+}
