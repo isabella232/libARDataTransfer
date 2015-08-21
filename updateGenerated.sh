b922d10484c879030237e11f9740ce8936ce8d40
Nicolas BRULEZ <nicolas.brulez@parrot.com>
2015-08-21 11:46:31 +0200

    [INTEGRATION] updateGenerated scrip exits with a non-zero value on error
    Change-Id: I87478b1cb15557ede7273b862244927afd7ae12e
    Reviewed-on: https://gerrit.parrot.biz/19657
    Reviewed-by: Pierre Zurek <pierre.zurek@parrot.com>

--- updateGenerated.sh
+++ updateGenerated.sh
@@ -1,1 +1,3 @@
-python ../ARSDKBuildUtils/Utils/Python/ARSDK_PrebuildActions.py --lib libARDataTransfer --root .
+#!/bin/sh
+
+python ../ARSDKBuildUtils/Utils/Python/ARSDK_PrebuildActions.py --lib libARDataTransfer --root . || exit 1
